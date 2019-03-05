8cf8981ca88e8818d75e2ba7236171d9ce3f95ca
Paul Honig <paul@etv.cx>
2019-03-06 00:25:42 +0100

    Add example 07 morse code.

---
+++ 07-case/chip.v
@@ -0,0 +1,28 @@
+
+module chip (
+	output	LED_R,
+	output	LED_G,
+	output	LED_B
+	);
+
+	wire clk, led_r, led_g, led_b;
+
+	SB_HFOSC u_hfosc (
+        	.CLKHFPU(1'b1),
+        	.CLKHFEN(1'b1),
+        	.CLKHF(clk)
+    	);
+
+	morse my_morse (
+		.clk(clk),
+		.rst(0),
+    		.led_r(led_r),
+    		.led_g(led_g),
+    		.led_b(led_b)
+	);
+
+	assign LED_R = led_r;
+	assign LED_G = led_g;
+	assign LED_B = led_b;
+
+endmodule
